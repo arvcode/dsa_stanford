086e605cecb681516c03d5cfe58ce8ab00764dcd
arv <s.arvindh@gmail.com>
2021-10-21 09:34:05 +0530

    Commit: Add Insertion & Selection sort
    [Description]
    1. Add elementary sorting algorithms.

--- elementary_sort.py
+++ elementary_sort.py
@@ -1,13 +1,13 @@
-#Insertion Sort & Selection sort
+#Insertion Sort & Selection sort -[Elementary sorts]
  
 # Compare elements with its predecessors and move it.
 
 def insertion_sort(arr):
 
-
+#Loop through the array
     for i in range(1,len(arr)):
         key=arr[i]
-
+#Loop through from 0 to i and swap the number to the correct place.
         for j in range(0,i):
             if (arr[j]>arr[i]):
                 (arr[j],arr[i])=(arr[i],arr[j])
@@ -15,10 +15,12 @@ def insertion_sort(arr):
     return arr
 
 
+#Select lowest element and place it at the right place.
 def selection_sort(arr):
 
-    for i in range(0,len(arr)):
-        low=i
+#Loop through the array
+    for i in range(0,len(arr)-1):
+#Loop through the array and swap the lowest number     
         for j in range(i+1,len(arr)):
             if arr[j]<arr[i]:
                 low=j
@@ -36,6 +38,9 @@ arr=[4,5,6,7,1,2,3,12]
 print(insertion_sort(arr))     
 
 arr=[1,2,3,4,5,6,7,100,99,89,56,34,45,67]
+print(insertion_sort(arr))
+
+arr=[10,9,8,7,6,5,4,3,2,1]
 print(insertion_sort(arr))
 
 print("Selection sort")
